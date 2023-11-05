504754a0b5879e8c89100557a7e4de509ebd9d82
sheik-mostafizur <sheikmostafizur2001@gmail.com>
2023-11-05 15:54:49 +0600

    change color with add cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,25 @@
+import {useState} from "react";
+import Navbar from "./components/Navbar";
+import Card from "./components/Card";
+
 function App() {
+  const [playlistId, setPlaylistId] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setPlaylistId(e.target.value);
+  };
   return (
-    <div className="bg-primary-50 shadow">
-      <div className="container mx-auto flex items-center justify-between px-8 py-4">
-        <h2 className="font-bold text-3xl text-primary-950">CleanYouTube</h2>
-        <form className="grid grid-cols-4 items-center justify-center gap-4">
-          <input
-            type="text"
-            id="default-input"
-            className="shadow col-span-3 bg-primary-50 border border-primary-300 text-primary-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-primary-700 dark:border-primary-600 dark:placeholder-primary-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-          />
-          <button
-            type="submit"
-            className=" text-white bg-gradient-to-r from-primary-500 via-primary-600 to-primary-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-primary-300 dark:focus:ring-primary-800 shadow-lg shadow-primary-500/50 dark:shadow-lg dark:shadow-primary-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 ">
-            Add Playlist
-          </button>
-        </form>
+    <div className="bg-[url('https://img.freepik.com/free-vector/gradient-glassmorphism-horizontal-banner_23-2149440108.jpg')] h-screen bg-no-repeat bg-cover bg-center overflow-y-auto">
+      <Navbar handleSubmit={handleSubmit} />
+      <div className="container mx-auto p-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-4 md:gap-6">
+          {Array(15)
+            .fill("card")
+            .map((item) => (
+              <Card />
+            ))}
+        </div>
       </div>
     </div>
   );
---
+++ src/components/Card.jsx
@@ -0,0 +1,32 @@
+const Card = () => {
+  return (
+    <div className="rounded overflow-hidden shadow-lg backdrop-blur hover:backdrop-blur-xl transition cursor-pointer hover:scale-105">
+      <img
+        className="w-full"
+        src="https://ergonotes.com/wp-content/uploads/2022/11/Find-YouTube-Thumbnail-Source.jpg"
+        alt="Sunset in the mountains"
+      />
+      <div className="px-6 py-4">
+        <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+        <p className="text-gray-700 text-base">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus
+          quia, nulla! Maiores et perferendis eaque, exercitationem praesentium
+          nihil.
+        </p>
+      </div>
+      <div className="px-6 pt-4 pb-2">
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+          #photography
+        </span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+          #travel
+        </span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+          #winter
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
